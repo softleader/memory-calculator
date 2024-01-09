ccc2b81c558461fa916f6d1def9323cf2249dcb3
Shihyu Ho <methodho@gmail.com>
2024-01-09 16:59:54 +0800

    feat: 調整 $MEMORY_CALCULATOR_HOME 取得邏輯

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-/tmp/memory-calculator -o /tmp/.env
+# 1. 先從 $1 參數取值
+BIN=${1:-}
 
-if [ -f "/tmp/.env" ]; then
-  source /tmp/.env
-  java -cp $( cat /app/jib-classpath-file ) $( cat /app/jib-main-class-file )
-else
-  java $JAVA_OPTS -cp $( cat /app/jib-classpath-file ) $( cat /app/jib-main-class-file )
+# 2. 若無指定, 判斷 MEMORY_CALCULATOR_HOME
+if [ -z "$BIN" ]; then
+  BIN=$MEMORY_CALCULATOR_HOME
 fi
 
+# 3. 如果都没有，使用預設 /usr/local/bin
+BIN=${BIN:-/usr/local/bin}
+
+TMP_ENV="/tmp/.env"
+
+$BIN/memory-calculator -o $TMP_ENV
+
+if [ -f "$TMP_ENV" ]; then
+  source $TMP_ENV
+fi
+
+java $JAVA_OPTS -cp $(cat /app/jib-classpath-file) $(cat /app/jib-main-class-file)
