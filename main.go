02c474f36d3fae3e4f9d2ed4bcfc26e377a6f886
Kyle Chen <kyle.chen@>
2024-01-10 03:19:53 +0800

    fix: default value

--- main.go
+++ main.go
@@ -91,19 +91,25 @@ func newConfig() Config {
 		applicationPath:   defaultApplicationPath,
 		memoryLimitPathV2: defaultMemoryLimitPathV2Fix,
 	}
-	c.jvmOptions, _ = os.LookupEnv(envJavaToolOptions)
+	if val, ok := os.LookupEnv(envJavaToolOptions); ok {
+		c.jvmOptions = val
+	}
 	if val, ok := os.LookupEnv(envBplJvmHeadRoom); ok {
 		c.headRoom, _ = strconv.Atoi(val)
 	}
 	if val, ok := os.LookupEnv(envBplJvmThreadCount); ok {
 		c.threadCount, _ = strconv.Atoi(val)
 	}
-	c.applicationPath, _ = os.LookupEnv(envBpiApplicationPath)
 	if val, ok := os.LookupEnv(envBpiJvmClassCount); ok {
 		c.loadedClassCount, _ = strconv.Atoi(val)
 	}
+	if val, ok := os.LookupEnv(envBpiApplicationPath); ok {
+		c.applicationPath = val
+	}
 	// 修正部分記憶體限制檔案位置不一致問題
-	c.memoryLimitPathV2, _ = os.LookupEnv(envBpiMemoryLimitPathV2)
+	if val, ok := os.LookupEnv(envBpiMemoryLimitPathV2); ok {
+		c.memoryLimitPathV2 = val
+	}
 	return c
 }
 
