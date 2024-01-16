697c3a45c71620e29fca54a8a57ecc1c29a1169e
Shihyu Ho <methodho@gmail.com>
2024-01-16 13:36:34 +0800

    docs: update demo

--- jib-demo/src/main/java/org/example/jibdemo/JibDemoApplication.java
+++ jib-demo/src/main/java/org/example/jibdemo/JibDemoApplication.java
@@ -1,12 +1,32 @@
 package org.example.jibdemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class JibDemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(JibDemoApplication.class, args);
+  }
+
+  @Bean
+  ApplicationRunner printEnv() {
+    return args -> {
+      log.info("=== System Env ===");
+      System.getenv().forEach((k, v) -> log.info("{}={}", k, v));
+
+      log.info("=== System Properties ===");
+      System.getProperties().forEach((k, v) -> log.info("{}={}", k, v));
+
+      log.info("=== Received {} Args ===", args.getSourceArgs().length);
+      for (String arg : args.getSourceArgs()) {
+        log.info("{}", arg);
+      }
+    };
   }
 }
