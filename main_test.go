4340533a2f9477ac3d2476232cd7afc7ac12f3dd
Shihyu Ho <methodho@gmail.com>
2024-01-20 10:48:31 +0800

    test: fix

--- main_test.go
+++ main_test.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-  "github.com/softleader/memory-calculator/calc"
-  "strings"
-  "testing"
+	"github.com/softleader/memory-calculator/calc"
+	"strings"
+	"testing"
 )
 
 func TestMemoryCalculation_WithLoadedClassCount(t *testing.T) {
-  calculator := calc.NewCalculator()
-  *calculator.LoadedClassCount = 1000
+	calculator := calc.NewCalculator()
+	*calculator.LoadedClassCount = 1000
 
-  jto, err := calculator.Execute()
-  if err != nil {
-    t.Fatalf("Execute returned an error: %v", err)
-  }
-  options := jto.String()
-  for _, o := range calc.ContributeOptions {
-    options = strings.ReplaceAll(options, o, "")
-  }
-  options = strings.ReplaceAll(options, calc.OptionsSeparator, "")
+	jto, err := calculator.Execute()
+	if err != nil {
+		t.Fatalf("Execute returned an error: %v", err)
+	}
+	options := jto.String()
+	for _, o := range calc.ContributeOptions {
+		options = strings.ReplaceAll(options, o, "")
+	}
+	options = strings.ReplaceAll(options, calc.OptionsSeparator, "")
 
-  if options == "" {
-    t.Errorf("Execute returned an empty string")
-  }
+	if options == "" {
+		t.Errorf("Execute returned an empty string")
+	}
 }
 
 func TestMemoryCalculation_WithoutLoadedClassCount(t *testing.T) {
-  calculator := calc.NewCalculator()
-  *calculator.AppPath = "."
-  *calculator.JVMClassCount = 1000
-  *calculator.JVMClassAdj = "150%"
+	calculator := calc.NewCalculator()
+	*calculator.AppPath = "."
+	*calculator.JVMClassCount = 1000
+	*calculator.JVMClassAdj = "10%"
 
-  jto, err := calculator.Execute()
-  if err != nil {
-    t.Fatalf("Execute returned an error: %v", err)
-  }
-  options := jto.String()
-  for _, o := range calc.ContributeOptions {
-    options = strings.ReplaceAll(options, o, "")
-  }
-  options = strings.ReplaceAll(options, calc.OptionsSeparator, "")
+	jto, err := calculator.Execute()
+	if err != nil {
+		t.Fatalf("Execute returned an error: %v", err)
+	}
+	options := jto.String()
+	for _, o := range calc.ContributeOptions {
+		options = strings.ReplaceAll(options, o, "")
+	}
+	options = strings.ReplaceAll(options, calc.OptionsSeparator, "")
 
-  if options == "" {
-    t.Errorf("Execute returned an empty string")
-  }
+	if options == "" {
+		t.Errorf("Execute returned an empty string")
+	}
 }
