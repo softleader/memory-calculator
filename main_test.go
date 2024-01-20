c55e755664b81486a9f47d1c2829efe860686019
Shihyu Ho <methodho@gmail.com>
2024-01-20 11:29:59 +0800

    test: update

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestMemoryCalculation_WithLoadedClassCount(t *testing.T) {
 	// 避免在不同環境中, 計算出來的結果超過那個環境實際的記憶體, 進而造成測試錯誤, 所以這邊我們假裝有足夠大的記憶體
-	file, err := createTempFile("10g")
+	file, err := createTempFileWithContent("10g")
 	if err != nil {
 		t.Fatalf("Failed to create temp file: %v", err)
 	}
@@ -38,7 +38,7 @@ func TestMemoryCalculation_WithLoadedClassCount(t *testing.T) {
 
 func TestMemoryCalculation_WithoutLoadedClassCount(t *testing.T) {
 	// 避免在不同環境中, 計算出來的結果超過那個環境實際的記憶體, 進而造成測試錯誤, 所以這邊我們假裝有足夠大的記憶體
-	file, err := createTempFile("10g")
+	file, err := createTempFileWithContent("10g")
 	if err != nil {
 		t.Fatalf("Failed to create temp file: %v", err)
 	}
@@ -66,12 +66,11 @@ func TestMemoryCalculation_WithoutLoadedClassCount(t *testing.T) {
 	}
 }
 
-func createTempFile(content string) (*os.File, error) {
+func createTempFileWithContent(content string) (*os.File, error) {
 	file, err := os.CreateTemp("", "testing-temp-file-")
 	if err != nil {
 		return nil, err
 	}
-
 	_, err = io.WriteString(file, content)
 	if err != nil {
 		return nil, err
